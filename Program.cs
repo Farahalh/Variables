3ed8945425d6fa95e7feb5f7aeddf7738e322f0d
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-08-31 15:49:21 +0200

    Continuing with different data types, variables and string concatenation

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*
             int x;
             int y;
 
@@ -11,6 +12,19 @@
             y = x + 3;
 
             Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + myFirstName + " " + myLastName);
             Console.ReadLine();
         }
     }
